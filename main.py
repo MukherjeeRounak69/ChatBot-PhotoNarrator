8523a3cf586167406517ce3e43cca7ec190c52f6
MukherjeeRounak69 <163704461+MukherjeeRounak69@users.noreply.github.com>
2024-07-07 21:05:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 import streamlit as st
 from streamlit_option_menu import option_menu
-from gemini_utility import (load_gemini_pro_model, gemini_pro_vision_response)
+from gemini_utility import (load_gemini_pro_model)
 import tensorflow as tf
 import numpy as np
 
@@ -15,64 +15,11 @@ st.set_page_config(
     layout="centered"
 )
 
-# Custom CSS for styling
-# st.markdown("""
-#     <style>
-#     .main {
-#         background-color: #f3f1fc;
-#         color: #333333;
-#     }
-#     .sidebar .sidebar-content {
-#         background-color: #e4e1f8;
-#         color: #333333;
-#     }
-#     .stTextInput > div > div > input {
-#         background-color: #ffffff;
-#         color: #000000;
-#     }
-#     .stButton button {
-#         background-color: #4CAF50;
-#         color: white;
-#     }
-#     .stFileUploader {
-#         color: #333333;
-#     }
-#     .css-1v3fvcr {
-#         background-color: #f3f1fc;
-#         color: #333333;
-#     }
-#     .css-1544g2n {
-#         background-color: #e4e1f8;
-#         color: #333333;
-#     }
-#     .css-1q5akcf {
-#         color: #000000;
-#     }
-#     .css-1d391kg p {
-#         color: #000000;
-#     }
-#     .css-145kmo2, .css-1d391kg, .css-1q5akcf, .css-1v3fvcr {
-#         color: #333333;
-#     }
-#     .css-1q1cmh1, .css-1lsmgbg, .css-1ko4ion {
-#         color: #333333;
-#     }
-#     .css-1d391kg {
-#         background-color: #ffffff;
-#         color: #333333;
-#     }
-#     .stButton button:hover {
-#         background-color: #45a049;
-#         color: white;
-#     }
-#     </style>
-# """, unsafe_allow_html=True)
-
 # Sidebar menu
 with st.sidebar:
-    selected = option_menu("RounakAI",
-                           ["ChatBot", "Image Captioning", "Plant Disease Prediction"],
-                           menu_icon='robot', icons=['chat-dots-fill', 'image-fill', 'leaf-fill'],
+    selected = option_menu("FloraCare",
+                           ["Plant Disease Prediction", "Find Cure"],
+                           menu_icon='robot', icons=['leaf-fill', 'chat-dots-fill'],
                            default_index=0)
 
 def translate_role_for_streamlit(user_role):
@@ -81,43 +28,9 @@ def translate_role_for_streamlit(user_role):
     else:
         return user_role
 
-# ChatBot functionality
-if selected == "ChatBot":
-    model = load_gemini_pro_model()
-    if "chat_session" not in st.session_state:
-        st.session_state.chat_session = model.start_chat(history=[])
-    st.title("🦾 ChatBot")
-    for message in st.session_state.chat_session.history:
-        with st.chat_message(translate_role_for_streamlit(message.role)):
-            st.markdown(message.parts[0].text)
-
-    user_prompt = st.chat_input("Ask Gemini Pro...")
-    if user_prompt:
-        st.chat_message("user").markdown(user_prompt)
-        gemini_response = st.session_state.chat_session.send_message(user_prompt)
-        with st.chat_message("assistant"):
-            st.markdown(gemini_response.text)
-
-# Image Captioning functionality
-elif selected == "Image Captioning":
-    st.title("📷 Photo Narrator")
-    uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
-    if uploaded_image and st.button("Generate Caption"):
-        image = Image.open(uploaded_image)
-        col1, col2 = st.columns(2)
-        with col1:
-            resized_img = image.resize((800, 500))
-            st.image(resized_img)
-
-        default_prompt = "Write a short caption for this image"
-        caption = gemini_pro_vision_response(default_prompt, image)
-        with col2:
-            st.info(caption)
-
-# Plant Disease Prediction functionality
-elif selected == "Plant Disease Prediction":
-    st.title("🌿 Plant Disease Classifier")
-    uploaded_image = st.file_uploader("Upload an image...", type=["jpg", "jpeg", "png"])
+if selected == "Plant Disease Prediction":
+    st.title("🌿 One Stop Health Check-Up For Your Plant Leaf!")
+    uploaded_image = st.file_uploader("Drop leaf image here...", type=["jpg", "jpeg", "png"])
 
     UPLOAD_FOLDER = 'uploads'
     if not os.path.exists(UPLOAD_FOLDER):
@@ -153,7 +66,7 @@ elif selected == "Plant Disease Prediction":
         image = Image.open(uploaded_image)
         st.image(image, caption='Uploaded Image', use_column_width=True)
 
-        if st.button("Predict Disease"):
+        if st.button("Test Your Leaf"):
             if 'model' in locals():
                 try:
                     preprocessed_image = load_and_prep_image(image)
@@ -171,3 +84,21 @@ elif selected == "Plant Disease Prediction":
                     st.error(f"Error processing image or making prediction: {e}")
             else:
                 st.error("Model not loaded.")
+                
+elif selected == "Find Cure":
+    model = load_gemini_pro_model()
+    if "chat_session" not in st.session_state:
+        st.session_state.chat_session = model.start_chat(history=[])
+    st.title("🩺 Leaf Doctor")
+    for message in st.session_state.chat_session.history:
+        with st.chat_message(translate_role_for_streamlit(message.role)):
+            st.markdown(message.parts[0].text)
+
+    user_prompt = st.chat_input("Ask Gemini Pro about Cure...")
+    if user_prompt:
+        st.chat_message("user").markdown(user_prompt)
+        gemini_response = st.session_state.chat_session.send_message(user_prompt)
+        with st.chat_message("assistant"):
+            st.markdown(gemini_response.text)
+
+
